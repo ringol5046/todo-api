e8e849a9ccb3bd2dcba84aba1da4d6a094ad7227
ringol5046 <ringol5046@gmail.com>
2017-09-17 12:42:03 -0500

    add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -23,17 +25,30 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({ todos });
   }, (e) => {
     res.status(400).send(e);
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) =>
+    res.status(400).send())
+})
+
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-module.exports = {app};
-
-
-
+module.exports = { app };
